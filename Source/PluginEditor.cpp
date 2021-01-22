92e22a4f18a613c2721af7425af9db4ff5c232a1
DeanT <dean.s.townshend@gmail.com>
2021-01-22 22:15:30 +0000

    changed the way the rotaries are drawn to go up odd/even

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -10,9 +10,9 @@ CassetteControlProcessorEditor::CassetteControlProcessorEditor(CassetteControlPr
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize(800, 400);
+    setSize(960, 600);
 
-    for (int i = 0; i < numOfNotes; i++)
+    for (int i = 0; i < 12; i++)
     {
         //Tuning sliders
         noteTuneSlider[i].addListener(this);
@@ -22,16 +22,16 @@ CassetteControlProcessorEditor::CassetteControlProcessorEditor(CassetteControlPr
         //noteTuneSlider[i].setColour(juce::Slider::textBoxBackgroundColourId, juce::Colours::slategrey);
         noteTuneSlider[i].setColour(juce::Slider::textBoxOutlineColourId, juce::Colours::ghostwhite);
         noteTuneSlider[i].setRange(0, 127, 1);
-        noteTuneSliderLabel[i].setJustificationType(juce::Justification::centred);
         noteTuneSlider[i].setValue(0.0);
-        //custLookFeel1.hello = 1;
-
+        
+        
+        //noteTuneSliderLabel[i].setJustificationType(juce::Justification::centred);
         //Tuning slider labels
-        noteTuneSliderLabel[i].setText("Note " + (juce::String)(i + 1), juce::dontSendNotification);
-        noteTuneSliderLabel[i].setColour(juce::Label::textColourId, juce::Colours::black);
+        //noteTuneSliderLabel[i].setText("Note " + (juce::String)(i + 1), juce::dontSendNotification);
+        //noteTuneSliderLabel[i].setColour(juce::Label::textColourId, juce::Colours::black);
 
         addAndMakeVisible(noteTuneSlider[i]);
-        addAndMakeVisible(noteTuneSliderLabel[i]);
+        //addAndMakeVisible(noteTuneSliderLabel[i]);
     }
 }
 
@@ -51,80 +51,73 @@ void CassetteControlProcessorEditor::resized()
 
     juce::Rectangle<int> sliderArea = area.removeFromTop(area.getHeight() * 0.4);
 
-    juce::Rectangle<int> upperSliderArea = sliderArea.removeFromTop(sliderArea.getHeight() * 0.5);
+    juce::Rectangle<int> upperSliderArea = sliderArea.removeFromTop(sliderArea.getHeight() / 2);
 
     juce::Rectangle<int> lowerSliderArea = sliderArea;
 
-    int numOfLowerNoteSliders = lowerSliderArea.getWidth()/13;
+    int cellWidth = lowerSliderArea.getWidth()/12;
 
-    std::array < juce::Rectangle<int>, 13> lowerSliderAreas;
-    //std::array < juce::Rectangle<int>, 13> lowerSliderLabelAreas;
-    std::array < juce::Rectangle<int>, 13> lowerSliderTxtAreas;
+    //lower
+    std::array < juce::Rectangle<int>, 12> lowerSliderAreas;
+    std::array < juce::Rectangle<int>, 12> lowerSliderTxtAreas;
 
-    for (int i = 0; i < 13; i++)
+    //upper
+    std::array < juce::Rectangle<int>, 12> upperSliderAreas;
+    std::array < juce::Rectangle<int>, 12> upperSliderTxtAreas;
+
+    for (int i = 0; i < 12; i++)
     {
-        lowerSliderAreas[i] = lowerSliderArea.removeFromLeft(numOfLowerNoteSliders);
-    }
-
-
-    for (int i = 0; i < 13; i++)
-    {
+        upperSliderAreas[i] = upperSliderArea.removeFromLeft(cellWidth);
+        lowerSliderAreas[i] = lowerSliderArea.removeFromLeft(cellWidth);
+        upperSliderTxtAreas[i] = upperSliderAreas[i].removeFromBottom(upperSliderAreas[i].getHeight() * 0.2);
         lowerSliderTxtAreas[i] = lowerSliderAreas[i].removeFromBottom(lowerSliderAreas[i].getHeight() * 0.2);
     }
 
-    for (int i = 0, ii = 0; i < 7; i++, ii+=2)
+    for (int i = 0; i < 12; i++)
     {
-        noteTuneSlider[i].setBounds(lowerSliderAreas[ii]);
-        noteTuneSlider[i].setTextBoxStyle(juce::Slider::TextBoxBelow, false, lowerSliderTxtAreas[ii].getWidth(), lowerSliderTxtAreas[ii].getHeight());
-        //noteTuneSliderLabel[i].setBounds(lowerSliderLabelAreas[ii]);
-    }
-
-    std::array < juce::Rectangle<int>, 13> upperSliderAreas;
-    std::array < juce::Rectangle<int>, 13> upperSliderLabelAreas;
-    std::array < juce::Rectangle<int>, 13> upperSliderTxtAreas;
-
-    for (int i = 0; i < 13; i++)
-    {
-        upperSliderAreas[i] = upperSliderArea.removeFromLeft(numOfLowerNoteSliders);
-    }
-
-
-    for (int i = 0; i < 13; i++)
-    {
-        upperSliderTxtAreas[i] = upperSliderAreas[i].removeFromBottom(upperSliderAreas[i].getHeight() * 0.2);
-    }
-
-    for (int i = 7, ii = 1, iii = 3; i < 12; i++, ii += 2, iii +=2)
-    {
-        if (i <=8)
+        if (i <= 4)
         {
-            noteTuneSlider[i].setBounds(upperSliderAreas[ii]);
-            noteTuneSlider[i].setTextBoxStyle(juce::Slider::TextBoxBelow, false, upperSliderTxtAreas[ii].getWidth(), upperSliderTxtAreas[ii].getHeight());
-            //noteTuneSliderLabel[i].setBounds(upperSliderLabelAreas[ii]);
+            if (i == 0 || i % 2 == 0)
+            {
+                noteTuneSlider[i].setBounds(lowerSliderAreas[i]);
+                noteTuneSlider[i].setTextBoxStyle(juce::Slider::TextBoxBelow, false, lowerSliderTxtAreas[i].getWidth(), lowerSliderTxtAreas[i].getHeight());
+            }
+            else if (i % 2 != 0)
+            {
+                noteTuneSlider[i].setBounds(upperSliderAreas[i]);
+                noteTuneSlider[i].setTextBoxStyle(juce::Slider::TextBoxBelow, false, upperSliderTxtAreas[i].getWidth(), upperSliderTxtAreas[i].getHeight());
+            }
         }
-        else if (i > 8)
+        else if(i > 4)
         {
-            noteTuneSlider[i].setBounds(upperSliderAreas[iii]);
-            noteTuneSlider[i].setTextBoxStyle(juce::Slider::TextBoxBelow, false, upperSliderTxtAreas[iii].getWidth(), upperSliderTxtAreas[iii].getHeight());
-            //noteTuneSliderLabel[i].setBounds(upperSliderLabelAreas[iii]);
+            if (i % 2 != 0)
+            {
+                noteTuneSlider[i].setBounds(lowerSliderAreas[i]);
+                noteTuneSlider[i].setTextBoxStyle(juce::Slider::TextBoxBelow, false, lowerSliderTxtAreas[i].getWidth(), lowerSliderTxtAreas[i].getHeight());
+            }
+            else if (i % 2 == 0)
+            {
+                noteTuneSlider[i].setBounds(upperSliderAreas[i]);
+                noteTuneSlider[i].setTextBoxStyle(juce::Slider::TextBoxBelow, false, upperSliderTxtAreas[i].getWidth(), upperSliderTxtAreas[i].getHeight());
+            }
+
         }
     }
 
-
-    juce::Rectangle<int> keyboardArea = area.removeFromTop(area.getHeight() * 0.4);
-        
-    juce::Rectangle<float> keyAreaFloat =  keyboardArea.toFloat();
-    keyboard.setAvailableRange(60, 72);
+    juce::Rectangle<int> keyboardArea = area.removeFromTop(area.getHeight() /4);    
     
     //keyboard.setSize(keyboardArea.getWidth(), keyboardArea.getHeight());
     //keyboard.setBoundsToFit(keyboardArea, juce::Justification::centred, true);
-
-
     //keyboard.setBoundsRelative(keyAreaFloat);
-    //keyboard.setBounds(keyAreaFloat);
+    keyboard.setKeyWidth(keyboardArea.getWidth() / 7);
+    keyboard.setBounds(keyboardArea);
+    keyboard.setAvailableRange(60, 72);
+    
+
+    //keyboard.setBoundsRelative(1, 1, keyAreaFloat.getWidth(), keyAreaFloat.getHeight());
 
     addAndMakeVisible(keyboard);
-    keyboard.setBoundsRelative(1, 1, keyAreaFloat.getWidth(), keyAreaFloat.getHeight());
+    
 }
 
 void CassetteControlProcessorEditor::sliderValueChanged(juce::Slider* slider)
